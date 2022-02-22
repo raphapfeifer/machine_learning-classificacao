c1c21d38f2cd4c73b4a623de975e54e6fdba633a
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-02-22 11:45:48 -0300

    algoritmo de teste

--- classificacao.py
+++ classificacao.py
@@ -12,12 +12,28 @@ dados = [porco1,porco2, porco3, cachorro1,
 
 marcacoes = [1,1,1,-1,-1,-1]
 
-misterioso = [1,1,1]
+misterioso1 = [1,1,1]
 misterioso2 = [1,0,0]
+misterioso3 = [0,0,1]
 
 #from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
-teste = [misterioso,misterioso2]
-print(modelo.predict(teste))
+teste = [misterioso1,misterioso2, misterioso3]
+
+marcacoes_teste = [-1,1,-1]
+
+resultado = modelo.predict(teste)
+diferencas = resultado - marcacoes_teste
+print(diferencas)
+
+acertos = [d for d in diferencas if d==0]
+print(acertos)
+total_de_acertos = len(acertos)
+print(total_de_acertos)
+total_de_elementos = len(teste)
+print(total_de_elementos)
+
+taxa_de_acerto = (total_de_acertos / total_de_elementos) * 100.0
+print(taxa_de_acerto)
