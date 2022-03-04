569a5f2d9fac6a4c3fe0ffbbd157ff83d88be6d6
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-03-04 16:31:10 -0300

    leitor de csv

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from classificacao_web import carregar_acessos
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def mostra_dados():
 
+    dados, marcacoes = carregar_acessos()
+    print('essas são as marcações {}'.format(marcacoes))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if(__name__== "__main__"):
+    mostra_dados()
