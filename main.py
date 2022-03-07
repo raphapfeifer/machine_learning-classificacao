864861095c7ec0f058e06b540974ae08f57e103f
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-03-07 15:18:35 -0300

    Testes de previsões

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from classificacao_web import carregar_acessos
+from sklearn.naive_bayes import MultinomialNB
 
 def mostra_dados():
 
     x, y = carregar_acessos()
-    print('essas são as marcações {}'.format(x))
+    #print('essas são as marcações {}'.format(x))
+
+    modelo = MultinomialNB()
+    modelo.fit(x,y)
+
+    #print(modelo.predict([[1,0,1],[0,1,0],[1,0,0],[1,1,0],[1,1,1]]))
+
+    resultado  = modelo.predict(x)
+    diferencas = resultado - y
+    acertos = [d for d in diferencas if d == 0]
+    total_acertos = len(acertos)
+    total_elementos = len(x)
+    taxa_de_acertos = 100.0 * total_acertos / total_elementos
+
+    print(taxa_de_acertos)
+    print(total_acertos)
 
 if(__name__== "__main__"):
     mostra_dados()
