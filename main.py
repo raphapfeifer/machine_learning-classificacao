b5a4f9a41e7accbbebc79555d277825bd431d4de
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-03-09 16:04:07 -0300

    Separação de mostras de treino e teste

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ from sklearn.naive_bayes import MultinomialNB
 def mostra_dados():
 
     x, y = carregar_acessos()
-    #print('essas são as marcações {}'.format(x))
+
+    treino_dados = x[:90]
+    treino_marcacoes = y[:90]
+
+    teste_dados = x[-9:]
+    teste_marcacoes = y[-9:]
+
 
     modelo = MultinomialNB()
-    modelo.fit(x,y)
+    modelo.fit(treino_dados,treino_marcacoes)
 
-    #print(modelo.predict([[1,0,1],[0,1,0],[1,0,0],[1,1,0],[1,1,1]]))
 
-    resultado  = modelo.predict(x)
-    diferencas = resultado - y
+
+    resultado  = modelo.predict(teste_dados)
+    diferencas = resultado - teste_marcacoes
     acertos = [d for d in diferencas if d == 0]
-    total_acertos = len(acertos)
+    total_de_acertos = len(acertos)
     total_elementos = len(x)
-    taxa_de_acertos = 100.0 * total_acertos / total_elementos
+    print("Total de elementos: {}".format(total_elementos))
+    taxa_de_acertos = 100.0 * total_de_acertos / total_elementos
 
     print(taxa_de_acertos)
-    print(total_acertos)
+    print(total_de_acertos)
 
 if(__name__== "__main__"):
     mostra_dados()
