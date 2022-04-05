7412e2cc9b0e44b07a5c919fd43096cdd3097b80
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-04-05 12:36:31 -0300

    Utilizando pandas para ler dados do .csv

--- rodar_buscas.py
+++ rodar_buscas.py
@@ -1,4 +1,19 @@
-from classifica_buscas import carregar_buscas
-x,y = carregar_buscas()
-print(x[0])
-print(y[0])
+#from classifica_buscas import carregar_buscas
+#x,y = carregar_buscas()
+#print(x[0])
+#print(y[0])
+
+import pandas as pd
+df = pd.read_csv('buscas.csv')
+
+x_df = df[['home', 'busca', 'logado']]
+y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(x_df)
+Ydummies_df = y_df
+
+x = Xdummies_df.values
+y = Ydummies_df.values
+
+print(y)
+
